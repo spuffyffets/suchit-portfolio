75f892a3e99a80c707bf83df174aba877efbc705
spuffyffets <suchitchaudhari17@gmamil.com>
2025-01-12 10:11:02 +0530

    p chnage

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Software Developer",
   description: "My name is Suchit Chaudhari. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on Java and I love to do most of the things using Java. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'suchitchaudhari17@gmail.com',
-  phone: '+918421519054',
+  phone: '+91999999998',
   address: 'Bavdan,Pune,India - 411021 ',
   github: 'https://github.com/spuffyffets',
   facebook: 'https://www.facebook.comd/suchit/',
