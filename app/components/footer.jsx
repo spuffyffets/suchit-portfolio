58b47a09cce609e38581527a5acee0d397152e7a
spuffyffets <suchitchaudhari17@gmamil.com>
2025-01-25 13:56:30 +0530

    1

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="www.linkedin.com/in/suchit-chaudhari" className="text-[#16f2b3]">Suchit Chaudhari</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/suchitchaudhari" className="text-[#16f2b3]">Suchit Chaudhari</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
